8668d539b70f18d0ed3d7e7dc4b9843f2f998522
moon <vkizim@gmail.com>
2022-02-13 21:08:42 +0200

    change index.js

--- src/index.js
+++ src/index.js
@@ -1,52 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: "" };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert("A name was submitted: " + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      key={props.value.toString()}
+      onClick={() => props.onClick()}
+    >
+      {props.value}
+    </button>
+  );
 }
+
+function Board({ dimension }) {
+  const status = "Следующий игрок: Х";
+  let a = [];
+  dimension = 3;
+  const [square, setSquare] = useState(Array(dimension * dimension).fill(null));
+  function handleClick(i) {
+    console.log(i);
+  }
+  function renderSquare(i) {
+    return <Square value={i} onClick={() => handleClick(i)} />;
+  }
+
+  for (let i = 0; i < dimension; i++) {
+    a.push([...Array(dimension).keys()].map((k) => k + i * dimension));
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      {a.map((raw) => (
+        <div className="board-row">{raw.map((i) => renderSquare(i))}</div>
+      ))}
+    </div>
+  );
+}
+
+function Game() {
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board />
+      </div>
+      <div className="game-info">
+        <div>{/* status */}</div>
+        <ol>{/* TODO */}</ol>
+      </div>
+    </div>
+  );
+}
+
+// ========================================
 
 ReactDOM.render(
   <React.StrictMode>
-    <NameForm />
+    <Game />
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
